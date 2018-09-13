d45eb7a967a3e0ec95190c12149c7f60b1fb886a
malygose_pro <malygose@gmail.com>
2018-09-14 01:10:16 +0900

    Swip refresh add.

--- app/src/main/java/com/niconicocbf/tokubailab/mvpdemo_cbf/view/activity/MainActivity.java
+++ app/src/main/java/com/niconicocbf/tokubailab/mvpdemo_cbf/view/activity/MainActivity.java
@@ -1,11 +1,13 @@
 package com.niconicocbf.tokubailab.mvpdemo_cbf.view.activity;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,10 +22,11 @@ import com.niconicocbf.tokubailab.mvpdemo_cbf.presenter.PhotoZoupicpresenter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BaseActivity<PhotoZoupicpresenter> implements DisplayView<List<PicInfo.InfoBean.PhotoBean>> {
+public class MainActivity extends BaseActivity<PhotoZoupicpresenter> implements DisplayView<List<PicInfo.InfoBean.PhotoBean>>, SwipeRefreshLayout.OnRefreshListener {
 
 
     private RecyclerView picRecyclerView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected PhotoZoupicpresenter createPresenter() {
@@ -39,17 +42,37 @@ public class MainActivity extends BaseActivity<PhotoZoupicpresenter> implements 
     protected void initView() {
         picRecyclerView = findViewById(R.id.recycler_view);
         picRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
-
         getmPresenter().getPicInfo();
-    }
+        mSwipeRefreshLayout = findViewById(R.id.swipe_refresh_widget);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setProgressViewOffset(false, 0, (int) TypedValue
+                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources()
+                        .getDisplayMetrics()));
 
+    }
 
     @Override
     public void getDataSuccess(final List<PicInfo.InfoBean.PhotoBean> successMsg) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                picRecyclerView.setAdapter(new PicListAdapter(getApplicationContext(),successMsg));
+                final PicListAdapter picListAdapter = new PicListAdapter(getApplicationContext(), successMsg);
+                picRecyclerView.setAdapter(picListAdapter);
+                picRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+
+                    @Override
+                    public void onScrollStateChanged(RecyclerView recyclerView,
+                                                     int newState) {
+                        super.onScrollStateChanged(recyclerView, newState);
+                        if (newState == RecyclerView.SCROLL_STATE_IDLE
+                                && 10 + 1 == picListAdapter.getItemCount()) {
+                            mSwipeRefreshLayout.setRefreshing(true);
+                            // 此处在现实项目中，请换成网络请求数据代码，sendRequest .....
+                           // handler.sendEmptyMessageDelayed(0, 3000);
+                        }
+
+
+                    }});
             }
         });
 
@@ -58,6 +81,11 @@ public class MainActivity extends BaseActivity<PhotoZoupicpresenter> implements 
 
     @Override
     public void getDataFail(List<PicInfo.InfoBean.PhotoBean> errorMsg) {
+
+    }
+
+    @Override
+    public void onRefresh() {
 
     }
 }
